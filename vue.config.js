8452bf2432df9836b04caae4932eadf4251fe116
codenarts84 <codenarts84@outlook.com>
2024-04-08 23:01:14 +0200

    change some icons 1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+module.exports = {
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+
+    svgRule.uses.clear();
+
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+  },
+};
