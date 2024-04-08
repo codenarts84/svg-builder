9b82a694af11990346fbe261088dba45de553fbd
hereistopdev <hereis.topdev@gmail.com>
2024-04-08 23:01:14 +0200

    change some icons 1

--- src/views/home/components/tools/MainTools.vue
+++ src/views/home/components/tools/MainTools.vue
@@ -3,33 +3,30 @@
     <v-btn class="btn" @click="$emit('addTextField')">
       <v-icon color="black" icon="mdi-cursor-default" size="large"></v-icon
     ></v-btn>
-    <v-btn class="btn" @click="$emit('addTextField')">
-      <v-icon
-        color="black"
-        icon="mdi-cursor-default-outline"
-        size="large"
-      ></v-icon
-    ></v-btn>
-    <v-btn class="btn" @click="$emit('addRectangle')"
-      ><v-icon color="black" icon="mdi-clipboard-multiple" size="large"></v-icon
-    ></v-btn>
+    <v-btn>
+      <DirectSelectionArrowIcon width="20px" height="20px" />
+    </v-btn>
+
+    <v-btn>
+      <CloneIcon width="20px" height="20px" />
+    </v-btn>
     <v-btn class="btn" @click="$emit('addRectangle')"
       ><v-icon color="black" icon="mdi-restore" size="large"></v-icon
     ></v-btn>
 
-    <v-btn class="btn" @click="$emit('addEllipse')"
-      ><v-icon
-        color="black"
-        icon="mdi-hand-back-right"
-        size="large"
-      ></v-icon></v-btn
-    ><v-btn class="btn" @click="onDelete"
+    <v-btn>
+      <HandRockIcon width="20px" height="20px" />
+    </v-btn>
+    <v-btn class="btn" @click="onDelete"
       ><v-icon color="black" icon="mdi-delete" size="large"></v-icon
     ></v-btn>
   </v-toolbar-items>
 </template>
 <script setup>
 import { defineProps } from "vue";
+import CloneIcon from "@/assets/svgs/menuIcons/CloneIcon.vue";
+import DirectSelectionArrowIcon from "@/assets/svgs/menuIcons/DirectSelectionArrowIcon.vue";
+import HandRockIcon from "@/assets/svgs/menuIcons/HandRockIcon.vue";
 
 const props = defineProps({
   removeItem: Function,
