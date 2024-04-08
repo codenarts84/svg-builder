0fb78f596c6aae1573da2a77d1cc4b97ade0557d
codenarts84 <codenarts84@outlook.com>
2024-04-08 23:09:58 +0200

    replace MainTools icons

--- src/views/home/components/tools/MainTools.vue
+++ src/views/home/components/tools/MainTools.vue
@@ -1,8 +1,9 @@
 <template>
   <v-toolbar-items class="border-left">
-    <v-btn class="btn" @click="$emit('addTextField')">
-      <v-icon color="black" icon="mdi-cursor-default" size="large"></v-icon
-    ></v-btn>
+    <v-btn>
+      <SelectionArrowIcon width="20px" height="20px" />
+    </v-btn>
+
     <v-btn>
       <DirectSelectionArrowIcon width="20px" height="20px" />
     </v-btn>
@@ -10,16 +11,16 @@
     <v-btn>
       <CloneIcon width="20px" height="20px" />
     </v-btn>
-    <v-btn class="btn" @click="$emit('addRectangle')"
-      ><v-icon color="black" icon="mdi-restore" size="large"></v-icon
-    ></v-btn>
+    <v-btn>
+      <UndoIcon width="20px" height="20px" />
+    </v-btn>
 
     <v-btn>
       <HandRockIcon width="20px" height="20px" />
     </v-btn>
-    <v-btn class="btn" @click="onDelete"
-      ><v-icon color="black" icon="mdi-delete" size="large"></v-icon
-    ></v-btn>
+    <v-btn class="btn" @click="onDelete">
+      <TrashIcon width="20px" height="20px"
+    /></v-btn>
   </v-toolbar-items>
 </template>
 <script setup>
@@ -27,6 +28,9 @@ import { defineProps } from "vue";
 import CloneIcon from "@/assets/svgs/menuIcons/CloneIcon.vue";
 import DirectSelectionArrowIcon from "@/assets/svgs/menuIcons/DirectSelectionArrowIcon.vue";
 import HandRockIcon from "@/assets/svgs/menuIcons/HandRockIcon.vue";
+import UndoIcon from "@/assets/svgs/menuIcons/UndoIcon.vue";
+import TrashIcon from "@/assets/svgs/menuIcons/TrashIcon.vue";
+import SelectionArrowIcon from "@/assets/svgs/menuIcons/SelectionArrowIcon.vue";
 
 const props = defineProps({
   removeItem: Function,
