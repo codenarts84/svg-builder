28ae1c75e818bf5e84972afa64387b4ef5633113
hereistopdev <hereis.topdev@gmail.com>
2024-04-10 01:44:45 +0200

    add hand tool

--- src/views/home/components/tools/MainTools.vue
+++ src/views/home/components/tools/MainTools.vue
@@ -12,8 +12,9 @@
     <v-btn>
       <UndoIcon width="20px" height="20px" />
     </v-btn>
-    <v-btn>
-      <HandRockIcon width="20px" height="20px" />
+    <v-btn @click="onHand">
+      <HandRockIcon v-if="hand_selected == true" width="20px" height="20px" />
+      <HandIcon v-if="hand_selected == false" width="20px" height="20px" />
     </v-btn>
     <v-btn class="btn" @click="onDelete">
       <TrashIcon width="20px" height="20px"
@@ -21,17 +22,24 @@
   </v-toolbar-items>
 </template>
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, ref, computed } from "vue";
 import CloneIcon from "@/assets/svgs/menuIcons/CloneIcon.vue";
 import DirectSelectionArrowIcon from "@/assets/svgs/menuIcons/DirectSelectionArrowIcon.vue";
 import HandRockIcon from "@/assets/svgs/menuIcons/HandRockIcon.vue";
 import UndoIcon from "@/assets/svgs/menuIcons/UndoIcon.vue";
 import TrashIcon from "@/assets/svgs/menuIcons/TrashIcon.vue";
 import SelectionArrowIcon from "@/assets/svgs/menuIcons/SelectionArrowIcon.vue";
+import HandIcon from "@/assets/svgs/menuIcons/HandIcon.vue";
+
+import { useBoardStore } from "@/stores/svgStore";
+
+const boardstore = useBoardStore();
+const hand_selected = ref(computed(() => boardstore.hand_selected));
 
 const props = defineProps({
   removeItem: Function,
 });
 
 const onDelete = () => props.removeItem();
+const onHand = () => boardstore.set_hand();
 </script>
