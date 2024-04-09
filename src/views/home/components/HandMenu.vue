eb9432b7833432d382e9660fa179e5ba4a7bafcc
codenarts84 <codenarts84@outlook.com>
2024-04-09 03:08:23 +0200

    make hand menu fixed

---
+++ src/views/home/components/HandMenu.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-list style="position: fixed; top: 55px; left: 5px">
+    <v-list-item @click="() => console.log('Hi')">
+      <v-list-item-title>
+        <AddChairTools />
+      </v-list-item-title>
+    </v-list-item>
+
+    <v-list-item @click="() => console.log('Hi')">
+      <v-list-item-title>
+        <AddItemsTools
+          :addTextField="addTextField"
+          :addEllipse="addEllipse"
+          :addRectangle="addRectangle"
+        />
+      </v-list-item-title>
+    </v-list-item>
+
+    <v-list-item @click="() => console.log('Hi')">
+      <v-list-item-title>
+        <AddAlignTools :textR="textR" :textL="textL" :textC="textC" />
+      </v-list-item-title>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+import AddChairTools from "./tools/AddChairTools.vue";
+import AddItemsTools from "./tools/AddItemsTools.vue";
+import AddAlignTools from "./tools/AddAlignTools.vue";
+
+// import ImportIcon from "@/assets/svgs/menuIcons/ImportIcon.vue";
+
+const props = defineProps({
+  addRectangle: Function,
+  addEllipse: Function,
+  addTextField: Function,
+  onImportClick: Function,
+  textR: Function,
+  textL: Function,
+  textC: Function,
+});
+
+console.log(props);
+</script>
