294c00c0e6effcfc8c95b95e5d223f7f6d2a6985
codenarts84 <codenarts84@outlook.com>
2024-04-28 14:22:02 -0700

    feat: Table Category

---
+++ src/views/layout/rightnav/CategoryTable.vue
@@ -0,0 +1,69 @@
+<template>
+  <div style="padding: 15px 10px; text-align: left">
+    <CategoryLabelModel />
+    <v-container>
+      <v-row style="display: flex; justify-content: center; align-items: center">
+        <v-col cols="12" sm="12">
+          <!-- <select class="v-custom-input" @input="setCategory">
+            <option v-for="op in category" :key="op" :value="op.name">
+              {{ op.name }}
+            </option>
+          </select> -->
+          <DropDown :selection="selectedItem" :options="category"
+            :setCategory="setCategory" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<style>
+.v-col-sm-6 {
+  padding: 5px;
+}
+</style>
+
+<script>
+import { defineComponent, computed, ref, withDirectives } from 'vue';
+import { usePlanStore } from '@/stores/plan';
+import CategoryLabelModel from './CategoryLabelModel.vue'
+import DropDown from '../../home/components/DropDown.vue';
+
+export default {
+  components: {
+    CategoryLabelModel,
+    DropDown
+  },
+  props: {
+    areas: Array,
+  },
+  data() {
+    return {
+    }
+  },
+  setup() {
+    const planStore = usePlanStore();
+    const category = computed(() => planStore.categories);
+    return {
+      planStore,
+      category
+    };
+  },
+  methods: {
+    setCategory(val) {
+      console.log(this.areas)
+      this.planStore.setTableCategory(this.areas, val)
+      // this.planStore.modifySeats({ seatIds: this.seats.map(s => s.uuid), category: val })
+
+    },
+  },
+  computed: {
+    selectedItem() {
+      if (this.category) {
+        return this.category.find(i => this.areas[0]?.category === i.name)
+      }
+      return ''
+    }
+  }
+};
+</script>
