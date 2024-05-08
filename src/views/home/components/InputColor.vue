16575658c94e2acf267fd99961270fa63365e546
codenarts84 <codenarts84@outlook.com>
2024-05-08 16:28:12 -0700

    bug/color-picker

--- src/views/home/components/InputColor.vue
+++ src/views/home/components/InputColor.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <input type="text" class="v-custom-input" @focus="show = true"
-      @blur="show = false" v-model="color" />
+    <input type="color" class="v-custom-input" @click="e => e.preventDefault()"
+      @focus="show = true" @blur="show = false" v-model="color" />
     <div class="d-relative">
       <v-color-picker v-show="show" class="v-color-picker" mode="hexa"
         v-model="color" />
