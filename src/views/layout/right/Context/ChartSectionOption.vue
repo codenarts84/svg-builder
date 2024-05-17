aed7a0f6cb3e5000e8a4f48d2e8e7f77eac63bd1
Lazar0314 <hereis.topdev@gmail.com>
2024-05-17 08:32:03 -0700

    fix/sections & categories

--- src/views/layout/right/Context/ChartSectionOption.vue
+++ src/views/layout/right/Context/ChartSectionOption.vue
@@ -1,13 +1,9 @@
 <template>
-  <div
-    style="padding: 15px 10px; text-align: left; display: flex; justify-content: space-between; align-items: center">
-    <h4>Sections</h4>
-    <v-btn class="btn" @click="dialog = true" size="small" variant="flat"
-      style="padding: 0px"><v-icon icon="mdi-cog"></v-icon>Manage
-    </v-btn>
+  <div style="padding: 15px 10px; text-align: left">
+    <SectionManage />
   </div>
 </template>
 
 <script setup>
-
+import SectionManage from "@/views/home/components/SectionManage.vue";
 </script>
